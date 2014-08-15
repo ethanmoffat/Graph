168bf98770179f1e4b6a9eea584328e8df9f0ea9
Ethan Moffat <ethanmoffat@hotmail.com>
2014-08-14 17:45:50 -0700

    Templates (part 1)
    Updating the class declarations and implementations to support templated
    parameters for vertices and edges. The internal structure is pretty much
    exactly the same, but instead of being an int** array, it is an edge**
    array. edge.distance provides the int value that would have been in the
    adjacency matrix originally.

--- DijkstraGraph/graph.h
+++ DijkstraGraph/graph.h
@@ -45,8 +45,12 @@ new:
 	use vertVals[1] to get the value of vertex 1 (templated)
 */
 
+template<typename T, typename U>
 class Graph
 {
+private:
+	struct edge; //fwd declaration
+
 public:
 	//(factory function) create undirected graph from 2D array
 	//This is designed this way because otherwise I would have had to template the entire
@@ -58,7 +62,7 @@ public:
 
 public: /* -- CONSTRUCTORS/DESTRUCTORS/ASSIGNMENT OPERATORS -- */
 	//default constructor: create an empty, undirected graph
-	Graph() : size(0), edgeCount(0), directed(false), adjList(NULL) { }
+	Graph() : size(0), edgeCount(0), directed(false), adjList(NULL), vertValues(NULL) { }
 	//creates a graph with the specified number of vertices but no edges
 	Graph(int numVerts, bool directed = false);
 	//copy from other graph
@@ -69,11 +73,13 @@ public: /* -- CONSTRUCTORS/DESTRUCTORS/ASSIGNMENT OPERATORS -- */
 
 public: /* -- MODIFIER METHODS -- */
 	//Adds a vertex to the graph (resizes internal array container). Returns the new vertex number.
-	int AddVertex();
+	int AddVertex(const T * const value = NULL);
 	//removes a vertex (resizes array) and any edges referencing it.
 	void RemoveVertex(int vertex);
+	//removes the first vertex (resizes array) with the value specified
+	void RemoveVertex(const T& value);
 	//adds an edge. Adds a bidirectional edge to an undirected graph (A->B and B->A)
-	void AddEdge(int vertexA, int vertexB, int weight = 1);
+	void AddEdge(int vertexA, int vertexB, int distance = 1, const U * const value = 0);
 	//removes an edge. removes both A->B and B->A in an undirected graph
 	void RemoveEdge(int vertexA, int vertexB);
 	//Removes all the edges. Used in GetShortestPathTree and GetMinimumSpanningTree
@@ -81,24 +87,31 @@ public: /* -- MODIFIER METHODS -- */
 	{
 		for (int i = 0; i < size; ++i)
 		{
-			memset(adjList[i], 0, sizeof(int)* size);
+			memset(adjList[i], 0, sizeof(edge)* size);
 		}
 		edgeCount = 0;
 	}
+
+	//add values for edges/vertices (useful when using CreateFromArray factory function)
+	void AddVertexValue(int vertValue, const T& value);
+	void AddEdgeValue(int vertAIndex, int vertBIndex, const U& value);
 
 public: /* -- TRAVERSAL METHODS -- */
 	//General parameters for these: specify the starting vertex and a 'visit' function
 	//The visit function is called on the vertex when the vertex is visited by the algorithm
 
 	//performs Breadth-first traversal (queue) on vertices
-	void BFS(const int startVert, const std::function<void(int)> &visit = [](int){}) const;
+	void BFS(const int startVert, const std::function<void(int, const T&)> &visit = [](int, T){}) const;
 	//performs Depth-first traversal (stack) on vertices
-	void DFS(const int startVert, const std::function<void(int)> &visit = [](int){}) const;
+	void DFS(const int startVert, const std::function<void(int, const T&)> &visit = [](int, T){}) const;
 
 	//Calculates Dijkstra's shortest-path-first algorithm from a starting vertex
 	//parameters for Dijkstra visit function are as follows: VertexNumber, DistancefromStart, list PathToVertex
 	void Dijkstra(const int startVert, 
-		const std::function<void(int, int, std::list<int>)> &visit = [](int, int, std::list<int>){}) const;
+		const std::function<void(int, const T&, int, std::list<std::pair<int, T>>)> &visit = [](int, const T&, int, std::list<std::pair<int,T>>){}) const;
+	//TODO: implement Dijkstra to call ShortestPath from startVert->endVert (so just do path between specified vertices)
+	void ShortestPath(const int startVert, const int endVert,
+		const std::function<void(int, const T&, int, std::list<std::pair<int, T>>)> &vist = [](int, const T&, int, std::list<std::pair<int, T>>){}) const;
 	//uses Dijkstra method to return a new graph containing a shortest path tree (returns as parameter)
 	void GetShortestPathTree(const int startVert, Graph &retGraph) const;
 	//Uses Prim's algorithm to find and return the minimum spanning tree graph. Returns by reference.
@@ -118,20 +131,43 @@ public: /* -- OTHER OPERATORS -- */
 	//This method uses red-blue coloring to determine if the graph is bipartite
 	bool IsBipartite() const;
 
+	//the following structs are internal structures for managing graph data
 private:
-	int **adjList; //adjacency matrix of edges
+	T * vertValues; //this is a lookup table that is sizeof(numVerts). Each T is the value of it's corresponding vertex
+	struct edge
+	{
+		int distance;
+		U value;
+
+		edge() : distance(0) { }
+		inline edge& operator=(const edge &rhs)
+		{
+			if (this == &rhs)
+				return *this;
+
+			this->distance = rhs.distance;
+			this->value = U(rhs.value);//invoke copy constructor
+			
+			return *this;
+		}
+	};
+
+private:
+	edge **adjList; //adjacency matrix of edges
 	int size, edgeCount; //size is number of vertices, edgeCount is number of edges
 	bool directed; //flag for whether or not the graph is directed; important only when AddEdge/RemoveEdge are called
 
 	//helper function for the dfs traversal: recursively called
-	void dfsHelper(const int startVert, bool * const visited, const std::function<void(int)> &visit) const;
+	void dfsHelper(const int startVert, bool * const visited, const std::function<void(int, const T&)> &visit) const;
+
 };
 
 //definition of static factory function
+template<typename T, typename U>
 template<int vertCount>
-static Graph* Graph::CreateFromArray(const int vertData[vertCount][vertCount])
+static Graph<T, U>* Graph<T, U>::CreateFromArray(const int vertData[vertCount][vertCount])
 {
-	Graph * ret = new Graph(vertCount);
+	Graph<T, U> * ret = new Graph<T, U>(vertCount);
 	for (int i = 0; i < vertCount; ++i)
 	{
 		//we only need to diagonally fill half the array, since the graph is undirected
@@ -142,5 +178,416 @@ static Graph* Graph::CreateFromArray(const int vertData[vertCount][vertCount])
 		}
 	}
 	return ret;
+}
+
+template<typename T, typename U>
+Graph<T, U>::Graph(int numVerts, bool directed)
+{
+	this->size = numVerts;
+	this->directed = directed;
+	this->edgeCount = 0;
+
+	if (size > 0)
+	{
+		this->adjList = new edge*[this->size];
+		this->vertValues = new T[this->size];
+
+		for (int i = 0; i < this->size; ++i)
+		{
+			this->adjList[i] = new edge[this->size];
+			this->vertValues[i] = T();
+			memset(this->adjList[i], 0, sizeof(edge)* this->size);
+		}
+	}
+	else
+	{
+		adjList = NULL;
+		vertValues = NULL;
+	}
+}
+
+template<typename T, typename U>
+Graph<T, U>::Graph(const Graph<T, U> &rhs)
+{
+	edgeCount = rhs.NumEdges();
+	size = rhs.NumVerts();
+	directed = rhs.IsDirectedGraph();
+
+	if (size > 0)
+	{
+		adjList = new edge*[size];
+		vertValues = new T[size];
+		for (int i = 0; i < size; ++i)
+		{
+			adjList[i] = new edge[size];
+			memcpy(adjList[i], rhs.adjList[i], sizeof(edge)* size);
+			vertValues[i] = T(rhs.vertValues[i]);
+		}
+	}
+	else
+	{
+		adjList = NULL;
+		vertValues = NULL;
+	}
+}
+
+template<typename T, typename U>
+Graph<T, U>& Graph<T, U>::operator=(const Graph<T, U> &rhs)
+{
+	if (&rhs == this)
+		return *this;
+
+	if (adjList != NULL)
+	{ //clear out this graph if there is data
+		for (int i = 0; i < size; ++i)
+			delete[] adjList[i];
+		delete[] adjList;
+		adjList = NULL;
+	}
+	if (vertValues != NULL)
+	{
+		delete[]vertValues;
+		vertValues = NULL;
+	}
+
+	edgeCount = rhs.NumEdges();
+	size = rhs.NumVerts();
+	directed = rhs.IsDirectedGraph();
+
+	if (size > 0)
+	{
+		adjList = new edge*[size];
+		vertValues = new T[size];
+		for (int i = 0; i < size; ++i)
+		{
+			adjList[i] = new edge[size];
+			memcpy(adjList[i], rhs.adjList[i], sizeof(edge)* size);
+			vertValues[i] = T(rhs.vertValues[i]);
+		}
+	}
+	else
+	{
+		adjList = NULL;
+		vertValues = NULL;
+	}
+
+	return *this;
+}
+
+template<typename T, typename U>
+Graph<T, U>::~Graph()
+{
+	if (adjList != NULL)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			//free each row
+			delete[] adjList[i];
+			adjList[i] = NULL;
+		}
+		//free the whole thing
+		delete[] adjList;
+		adjList = NULL;
+	}
+
+	if (vertValues != NULL)
+	{
+		delete[] vertValues;
+		vertValues = NULL;
+	}
+}
+
+template<typename T, typename U>
+int Graph<T, U>::AddVertex(const T * const value)
+{
+	edge ** oldAdjList = NULL;
+	T * oldVertValues = NULL;
+
+	if (size > 0)
+	{//pointers to old data
+		oldAdjList = adjList; 
+		oldVertValues = vertValues;
+	}
+	size++;
+	adjList = new edge*[size]; //allocate new memory
+	vertValues = new T[size];
+
+	for (int i = 0; i < size; ++i)
+	{
+		adjList[i] = new edge[size]; //allocate each row
+		memset(adjList[i], 0, sizeof(edge)* size); //set to 0
+	}
+
+	if (oldAdjList != NULL)
+	{
+		for (int i = 0; i < size - 1; ++i)
+		{
+			memcpy(adjList[i], oldAdjList[i], sizeof(edge)* (size - 1)); //copy the memory over
+			delete[] oldAdjList[i]; //delete the old memory
+			oldAdjList[i] = NULL;
+		}
+		delete[] oldAdjList; //free the whole of the old memory
+		oldAdjList = NULL;
+	}
+
+	if (oldVertValues != NULL)
+	{
+		memcpy(vertValues, oldVertValues, sizeof(T)* (size - 1));
+		delete[] oldVertValues;
+		oldVertValues = NULL;
+	}
+
+	if (value != NULL)
+	{
+		vertValues[size - 1] = *value;
+	}
+	else
+	{
+		vertValues[size - 1] = T();
+	}
+
+	return size - 1; //new highest index is the old size
+}
+
+template<typename T, typename U>
+void Graph<T, U>::RemoveVertex(int vertex)
+{
+	if (vertex < 0 || vertex >= size || size == 0)
+		return;
+
+	edge ** oldData = adjList;
+	T * oldVertValues = vertValues;
+	size--;
+
+	if (size > 0)
+	{
+		adjList = new edge*[size];
+		vertValues = new T[size];
+		for (int src = 0, dst = 0; dst < size; ++dst, ++src)
+		{ //foreach row: skip over vertex row, copy otherwise
+			if (src == vertex)
+			{//skip source row, but still copy over for this iteration (for dest)
+				delete[]oldData[src];
+				oldData[src] = NULL;
+				src++;
+			}
+			adjList[dst] = new edge[size];
+
+			for (int row = 0, off = 0; (row - off) < size; ++row)
+			{ //for this row: skip over the vertex, copy otherwise
+				if (row == vertex)
+				{
+					if (oldData[src][row].distance > 0)
+						edgeCount--;
+					off = 1;
+					row++;
+				}
+				adjList[dst][row - off] = oldData[src][row];
+			}
+			delete[] oldData[src];
+			oldData[src] = NULL;
+
+			vertValues[dst] = T(oldVertValues[src]);
+		}
+		delete[] oldVertValues;
+		oldVertValues = NULL;
+	}
+	else
+	{ //if we're deleting the last vertex things are very easy
+		delete[] adjList[0];
+		delete[] adjList;
+		delete[] vertValues;
+		adjList = NULL;
+		vertValues = NULL;
+	}
+}
+
+template<typename T, typename U>
+void Graph<T, U>::RemoveVertex(const T &value)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		if (value == vertValues[i])
+		{
+			//This is an easy wrapper using a sequential search to find the
+			//	first matching vertex value
+			RemoveVertex(i);
+			break;
+		}
+	}
+}
+
+template<typename T, typename U>
+void Graph<T, U>::AddEdge(int vertexA, int vertexB, int distance, const U * const value)
+{
+	//The following condition implies that A and B must both be created vertices
+	//	in order to add an edge between them. Adding an edge DOES NOT manipulate the 
+	//	number of vertices in the graph.
+	if (vertexA < 0 || vertexA >= size || //vertex A must be between 0 and size - 1
+		vertexB < 0 || vertexB >= size || //vertex B must be between 0 and size - 1
+		distance < 1 || //weight must be greater than 0 (adding an edge implies we are not removing it - a zero would remove it
+		vertexA == vertexB) //don't allow edges to point from A->A (I don't want to support this case)
+		return;
+
+	if (adjList[vertexA][vertexB].distance == 0)
+		edgeCount++;
+
+	edge newEdge;
+	newEdge.distance = distance;
+	if (value != NULL)
+		newEdge.value = *value;
+
+	adjList[vertexA][vertexB] = newEdge;
+	if (!directed)
+		adjList[vertexB][vertexA] = newEdge;
+}
+
+template<typename T, typename U>
+void Graph<T, U>::RemoveEdge(int vertexA, int vertexB)
+{
+	if (vertexA < 0 || vertexA >= size ||
+		vertexB < 0 || vertexB >= size || 
+		vertexA == vertexB || edgeCount == 0) //similar restraints for removing the edge
+		return;
+
+	adjList[vertexA][vertexB] = edge();
+	edgeCount--;
+	if (!directed)
+		adjList[vertexB][vertexA] = edge();
+}
+
+template<typename T, typename U>
+void Graph<T, U>::AddVertexValue(int vertIndex, const T& value)
+{
+	if (vertIndex < 0 || vertIndex >= size)
+		return;
+
+	vertValues[vertIndex] = value;
+}
+
+template<typename T, typename U>
+void Graph<T, U>::AddEdgeValue(int vertAIndex, int vertBIndex, const U& value)
+{
+	if (vertAIndex < 0 || vertAIndex >= size || vertBIndex < 0 || vertBIndex >= size)
+		return;
+
+	adjList[vertAIndex][vertBIndex].value = value;
+	if (!directed)
+		adjList[vertBIndex][vertAIndex].value = value;
+}
+
+template<typename T, typename U>
+void Graph<T, U>::BFS(const int startVert, const std::function<void(int, const T&)> &visit) const
+{ //BFS traversal algorithm as described in slides
+	if (startVert < 0 || startVert >= size || size == 0 || edgeCount == 0)
+		return;
+
+	std::queue<int> toVisit; //use queue to track vertices we want to visit
+	bool * visited = new bool[size];
+	memset(visited, 0, sizeof(bool)* size);
+
+	toVisit.push(startVert);
+	visited[startVert] = true;
+	while (toVisit.size() > 0)
+	{
+		int curVert = toVisit.front();
+		toVisit.pop();
+
+		for (int i = 0; i < size; ++i)
+		{
+			if (adjList[curVert][i].distance > 0 && !visited[i])
+			{
+				toVisit.push(i);
+				visited[i] = true;
+			}
+		}
+
+		visit(curVert, vertValues[curVert]);
+	}
+	delete[] visited;
+}
+
+template<typename T, typename U>
+void Graph<T, U>::dfsHelper(const int startVert, bool * const visited, const std::function<void(int, const T&)> &visit) const
+{
+	visited[startVert] = true;
+	visit(startVert, vertValues[startVert]);
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (adjList[startVert][i].distance > 0 && !visited[i])
+			dfsHelper(i, visited, visit);
+	}
+}
+
+template<typename T, typename U>
+void Graph<T, U>::DFS(const int startVert, const std::function<void(int, const T&)> &visit) const
+{ //DFS traversal implemented as discussed in class
+	if (startVert < 0 || startVert >= size || size == 0 || edgeCount == 0)
+		return;
+
+	bool * const visited = new bool[size];
+	memset(visited, 0, sizeof(bool)* size);
+	dfsHelper(startVert, visited, visit); //use the call stack as a stack container for DFS (recursion)
+	delete[] visited;
+}
+
+template<typename T, typename U>
+void Graph<T, U>::Dijkstra(const int startVert, const std::function<void(int, const T&, int, std::list<std::pair<int, T>>)> &visit) const
+{
+	if (startVert < 0 || startVert >= size || size == 0 || edgeCount == 0)
+		return;
+
+	//set up distance array
+	int * dist = new int[size];
+	for (int i = 0; i < size; ++i) dist[i] = 0x7FFFFFFF;//distance of 0x7FFFFFFF is INFINITE, same as MAX_INT
+	dist[startVert] = 0; //initial vertex has distance of 0
+	
+	bool * spt = new bool[size]; //spt is the list of vertices that are finalized. Set to true when done
+	memset(spt, false, sizeof(bool)* size); //initially they are all false
+	
+	std::list<std::pair<int, T>> * pathList = new std::list<std::pair<int, T>>[size]; //create an array of lists: each list contains the path
+	for (int i = 0; i < size; ++i)
+	{
+		pathList[i].push_back(pair<int, T>(startVert, vertValues[startVert])); //each list starts at startVert
+	}
+
+	for (int i = 0; i < size - 1; ++i) //iterate over all vertices
+	{
+		int curVert, min = 0x7FFFFFFF; //min is used only the the next loop to search sequentially for the vertex with the minimum distance
+		for (int j = 0; j < size; ++j) //find the minimum unchecked vertex
+		{
+			if (!spt[j] && dist[j] <= min)
+			{
+				min = dist[j];
+				curVert = j;
+			}
+		}
+
+		//curVert always equals startVert on the first iteration through
+		spt[curVert] = true;
+		for (int j = 0; j < size; j++)
+		{
+			if (adjList[curVert][j].distance == 0) //j is not an adjacent vertex
+				continue;
+
+			if (!spt[j] && dist[curVert] != 0x7FFFFFFF && 
+				dist[curVert] + adjList[curVert][j].distance < dist[j])
+			{
+				//update the distance and the paths for vertex J
+				dist[j] = dist[curVert] + adjList[curVert][j].distance;
+				
+				pathList[j].clear();
+				for (std::list<std::pair<int, T>>::iterator iter = pathList[curVert].begin(); iter != pathList[curVert].end(); ++iter)
+					pathList[j].push_back(*iter);
+				pathList[j].push_back(std::pair<int, T>(j, vertValues[j])); //push the current vertex
+			}
+		}
+	}
+
+	for (int i = 0; i < size; ++i) visit(i, vertValues[i], dist[i], pathList[i]); //visit each vertex
+	delete[] dist;
+	delete[] spt;
+	delete[] pathList;
 }
 #endif
--- DijkstraGraph/main.cpp
+++ DijkstraGraph/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <string>
 #include "graph.h"
 
 using namespace std;
@@ -36,16 +37,17 @@ const int slides[GRAPH_SIZE][GRAPH_SIZE] = {
 	{ 1,0,0,0,0,0,0,0,0 }
 	}; //this is a representation of the graph from the chapter 3 slides (for testing DFS/BFS)
 
+const char slideVertVals[GRAPH_SIZE] = { 'A', 'b', 'C', 'd', 'E', 'f', 'G', 'h', 'I' };
+
 int printVert(int vertex)
 { //generic print function. prints the node.
 	cout << "Visited node: " << vertex << endl;
 	return 0;
 }
 
-int printVertAlpha(int vertex)
+void printVertAlpha(int vertex, int value)
 { //converts the node to a letter representation starting from 'a'.
 	cout << "Visited node: " << (char)(vertex + 'a') << endl;
-	return 0;
 }
 
 int printPlus1(int vertex)
@@ -54,10 +56,10 @@ int printPlus1(int vertex)
 	return 0;
 }
 
-void CreateMSTTest(Graph &g) //making an adjacency matrix for this would have been hell. use the methods in Graph class to generate it.
+void CreateMSTTest(Graph<int, int> &g) //making an adjacency matrix for this would have been hell. use the methods in Graph class to generate it.
 {// see http://www.me.utexas.edu/~jensen/exercises/mst_spt/mst_demo/mst1.html for more info
 	if (g.NumVerts() != 11)
-		g = Graph(11); //we have 11 nodes in the graph
+		g = Graph<int, int>(11); //we have 11 nodes in the graph
 
 	g.AddEdge(0,1,8); g.AddEdge(0,6,9); g.AddEdge(0,7,10); g.AddEdge(0,8,6); g.AddEdge(0,9,12); g.AddEdge(0,10,3);
 	g.AddEdge(1,2,10); g.AddEdge(1,4,2); g.AddEdge(1,10,7);
@@ -71,34 +73,61 @@ void CreateMSTTest(Graph &g) //making an adjacency matrix for this would have be
 	g.AddEdge(9,10,8);
 }
 
-void TestFromArray();
-void TestMST();
-void TestStupidCast();
+//void TestFromArray();
+//void TestMST();
+//void TestStupidCast();
 
 int main(int c, char * argv[])
-{
+{/*
 	TestFromArray();
 	TestMST();
-	TestStupidCast();
-	return 0;
-}
+	TestStupidCast();*/
 
-void TestFromArray()
-{
-	Graph * g = NULL;
-	g = Graph::CreateFromArray<GRAPH_SIZE>(graphData);
+	Graph<char, string> g;
 
-	for (int i = 0; i < g->NumVerts(); ++i)
+	char val = '0';
+	g.AddVertex(&val);
+	g.AddVertex();
+
+	string edgeVal = "hello world";
+	g.AddEdge(0, 1, 5, &edgeVal);
+	g.RemoveEdge(0, 1);
+	g.RemoveVertex('0');
+	g.RemoveVertex(0);
+
+	Graph<double, int> g2(4, false);
+	Graph<double, int> g3(g2);
+
+	Graph<char, string> g4;
+	g4 = g;
+	g4.RemoveAllEdges();
+	
+	Graph<char, int> * test = Graph<char, int>::CreateFromArray<GRAPH_SIZE>(slides);
+	for (int i = 0; i < test->NumVerts(); ++i)
+	{
+		test->AddVertexValue(i, slideVertVals[i]);
+	}
+
+
+	for (int i = 0; i < test->NumVerts(); ++i)
 	{ //run Dijkstra's algorithm from every starting vertex
 		cout << "Running Dijkstra's algorithm starting from: " << i << endl;
 		cout << left << setw(8) << "Vertex" << setw(10) << "Distance" << "Path" << endl;
-		g->Dijkstra(i, [](int vert, int dist, list<int> pathInfo) //lambda expression for visit function
+		test->Dijkstra(i, [](int vert, const int &value, int dist, list<pair<int,char>> pathInfo) //lambda expression for visit function
 		{
-			cout << setw(8) << left << vert << setw(10) << dist; //vertex/distance
-			list<int>::iterator iter = pathInfo.begin();
+			//put the vertex and the value into a string to print out
+			char turd[10];
+			_itoa_s(vert, turd, 10);
+			string vertplusval = turd;
+			vertplusval += "(";
+			vertplusval += value;
+			vertplusval += ")";
+
+			cout << setw(8) << left << vertplusval << setw(10) << dist; //vertex/distance
+			list<pair<int, char>>::iterator iter = pathInfo.begin();
 			while (iter != pathInfo.end()) //iterate over the path list and print the values
 			{
-				cout << *iter;
+				cout << iter->first << "(" << iter->second << ")";
 				if (++iter != pathInfo.end())
 					cout << "-->"; //separate with the arrow
 			}
@@ -108,48 +137,78 @@ void TestFromArray()
 		cout << endl << endl; //spacing between calls to g->Dijkstra
 	}
 
-	delete g; //the factory function CreateFromArray dynamically allocates memory.
+	delete test;
+
+	return 0;
 }
-
-void TestMST()
-{
-	//The following is a demo of the MST algorithm
-	
-	Graph mstStart(11), mst;
-	CreateMSTTest(mstStart);
-	mstStart.GetMinimumSpanningTree(mst);
-	cout << endl;
-	cout << "starting graph " << (mstStart.IsBipartite() ? "is" : "is not") << " bipartite." << endl;
-	cout << "MST      graph " << (mst.IsBipartite() ? "is" : "is not") << " bipartite." << endl;
-	cout << "Edge count: " << mst.NumEdges() << " | Vertex count: " << mst.NumVerts() << endl;
-	cout << mst;
-}
-
-void TestStupidCast()
-{
-	const char * const str1 = "Edge One";
-	const char * const str2 = "Edge Two";
-	const char * const str3 = "Edge Three";
-	const char * const str4 = "Edge Four";
-
-	Graph g(5, false);
-
-	g.AddEdge(0, 1, reinterpret_cast<int>(str1));
-	g.AddEdge(1, 2, reinterpret_cast<int>(str2));
-	g.AddEdge(2, 3, reinterpret_cast<int>(str3));
-	g.AddEdge(3, 4, reinterpret_cast<int>(str4));
-
-	cout << "Trying to get edges back from graph..." << endl;
-	for (int i = 0; i < g.NumVerts(); ++i)
-	{
-		const int * const vert = g[i];
-		if (vert == NULL)
-			continue;
-
-		for (int j = i; j < g.NumVerts(); ++j)
-		{
-			char* edgeLabel = reinterpret_cast<char*>(vert[j]);
-			cout << (edgeLabel ? edgeLabel : "") << (edgeLabel ? "\n" : "");
-		}
-	}
-}
+//
+//void TestFromArray()
+//{
+//	Graph * g = NULL;
+//	g = Graph::CreateFromArray<GRAPH_SIZE>(graphData);
+//
+//	for (int i = 0; i < g->NumVerts(); ++i)
+//	{ //run Dijkstra's algorithm from every starting vertex
+//		cout << "Running Dijkstra's algorithm starting from: " << i << endl;
+//		cout << left << setw(8) << "Vertex" << setw(10) << "Distance" << "Path" << endl;
+//		g->Dijkstra(i, [](int vert, int dist, list<int> pathInfo) //lambda expression for visit function
+//		{
+//			cout << setw(8) << left << vert << setw(10) << dist; //vertex/distance
+//			list<int>::iterator iter = pathInfo.begin();
+//			while (iter != pathInfo.end()) //iterate over the path list and print the values
+//			{
+//				cout << *iter;
+//				if (++iter != pathInfo.end())
+//					cout << "-->"; //separate with the arrow
+//			}
+//			cout << endl; //endline after the visit function is done
+//		});
+//
+//		cout << endl << endl; //spacing between calls to g->Dijkstra
+//	}
+//
+//	delete g; //the factory function CreateFromArray dynamically allocates memory.
+//}
+//
+//void TestMST()
+//{
+//	//The following is a demo of the MST algorithm
+//	
+//	Graph mstStart(11), mst;
+//	CreateMSTTest(mstStart);
+//	mstStart.GetMinimumSpanningTree(mst);
+//	cout << endl;
+//	cout << "starting graph " << (mstStart.IsBipartite() ? "is" : "is not") << " bipartite." << endl;
+//	cout << "MST      graph " << (mst.IsBipartite() ? "is" : "is not") << " bipartite." << endl;
+//	cout << "Edge count: " << mst.NumEdges() << " | Vertex count: " << mst.NumVerts() << endl;
+//	cout << mst;
+//}
+//
+//void TestStupidCast()
+//{
+//	const char * const str1 = "Edge One";
+//	const char * const str2 = "Edge Two";
+//	const char * const str3 = "Edge Three";
+//	const char * const str4 = "Edge Four";
+//
+//	Graph g(5, false);
+//
+//	g.AddEdge(0, 1, reinterpret_cast<int>(str1));
+//	g.AddEdge(1, 2, reinterpret_cast<int>(str2));
+//	g.AddEdge(2, 3, reinterpret_cast<int>(str3));
+//	g.AddEdge(3, 4, reinterpret_cast<int>(str4));
+//
+//	cout << "Trying to get edges back from graph..." << endl;
+//	for (int i = 0; i < g.NumVerts(); ++i)
+//	{
+//		const int * const vert = g[i];
+//		if (vert == NULL)
+//			continue;
+//
+//		for (int j = i; j < g.NumVerts(); ++j)
+//		{
+//			char* edgeLabel = reinterpret_cast<char*>(vert[j]);
+//			cout << (edgeLabel ? edgeLabel : "") << (edgeLabel ? "\n" : "");
+//		}
+//	}
+//}
